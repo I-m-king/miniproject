24ea392e67ffddc5011d18c6b59e498c347442b1
GI_WON <rldnjsqhdl@naver.com>
2023-04-24 18:28:53 +0900

    기원3

--- src/com/gomangoman/member/login/Login.java
+++ src/com/gomangoman/member/login/Login.java
@@ -19,32 +19,26 @@ public class Login {
 		System.out.print("비밀번호를 입력해주세요 : ");
 		String pwd = sc.nextLine();
 		
-		if(id.equals(suspervisor.getId())) {
-			if(pwd.equals(suspervisor.getPwd())) {
-				System.out.print("로그인이 완료되었습니다.");
-				break label ;
-			}else
-				System.out.print("비밀번호가 틀렸습니다. 다시 입력해주세요. 다시 입력하시겠습니까 ? Y/N ");
-				char c = sc.next().charAt(0);
-				if (c =='Y' || c == 'y') {
-					break;
-				}else {
-			    break label;
-				}
-					
+		if(suspervisor.equals(id)&&suspervisor.equals(pwd)) {
+			System.out.println("로그인에 성공했습니다.");
 		}else
-			System.out.print("아이디가 틀렸습니다. 다시 입력하시겠습니까 ? Y/N.");
-			char c2 = sc.next().charAt(0);
-			if (c2 =='Y' || c2 == 'y') {
+			System.out.print("로그인에 실패했습니다 다시 입력하시겠습니까? Y/N");
+			char c = sc.next().charAt(0);
+			if(c == 'y' || c == 'Y') {
 				break;
-			}else {
-		    break label;
 			}
-		
-		
+			if(c == 'c' || c == 'C') {
+				break label;
+			}
 	}
+     
+	
+	
 	}
+		
+	
 
 }
+      
 
 
