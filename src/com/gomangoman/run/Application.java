51f140496fb893e62fbdc1de3c14a95d3b2c9ef6
jihopark <jihopark@DESKTOP-TFLQRBN>
2023-04-24 17:44:20 +0900

    first commit

--- src/com/gomangoman/run/Application.java
+++ src/com/gomangoman/run/Application.java
@@ -1,5 +1,14 @@
 package com.gomangoman.run;
 
+import com.gomangoman.main.StartMenu;
+
 public class Application {
+	
+	public static void main(String[] args) {
+		
+		StartMenu menu = new StartMenu();
+		
+		menu.mainMenu();
+	}
 
 }
