aa65c5fa05c2a4c91450cd0e59058e2b95201343
GI_WON <rldnjsqhdl@naver.com>
2023-04-25 19:13:12 +0900

    기우너5

--- src/com/gomangoman/member/dto/GuestDTO.java
+++ src/com/gomangoman/member/dto/GuestDTO.java
@@ -1,12 +1,18 @@
 package com.gomangoman.member.dto;
 
+import java.util.ArrayList;
+
+import com.gomangoman.inventory.ProductDTO;
+
 public class GuestDTO {
 	
 	private String name;
 	private int phone;
 	private int money;
 	private double point;
-//	productDTO 배열
+	
+	private ArrayList<ProductDTO> product;
+
 	public GuestDTO() {
 	}
 	public GuestDTO(String name, int phone, int money) {
@@ -43,6 +49,12 @@ public class GuestDTO {
 		this.point = point;
 	}
 	
+	public ArrayList<ProductDTO> getProduct() {
+		return product;
+	}
+	public void setProduct(ArrayList<ProductDTO> product) {
+		this.product = product;
+	}
 	@Override
 	public String toString() {
 		return "성함은 " + name + "입니다." + ", 전화번호 뒷자리는 " + phone + "입니다" + "갖고있는 돈은" + money + "입니다" +"적립된 포인트는" + point +" 입니다";
--- src/com/gomangoman/member/login/Login.java
+++ src/com/gomangoman/member/login/Login.java
@@ -10,14 +10,14 @@ public class Login {
 	Scanner sc = new Scanner(System.in);
 	
 	SupervisorDTO suspervisor = new SupervisorDTO();
-	
+	int count = 0;
 	
 	public void login () {
       
-		int count = 0;
+		
 		label :
     
-		while(count>5) {
+		while(count<5) {
 		System.out.print("아이디를 입력주세요 : ");
 		String id = sc.nextLine();
 		System.out.print("비밀번호를 입력해주세요 : ");
@@ -26,14 +26,16 @@ public class Login {
 		if(id.equals(suspervisor.getId())&&pwd.equals(suspervisor.getPwd())) {
 			System.out.println("로그인에 성공했습니다.");
 		}else
-			System.out.print("로그인에 실패했습니다 다시 입력하시겠습니까? Y/N");
+			
+			System.out.print("로그인에 실패했습니다 다시 입력하시겠습니까? Y/N : ");
 			char c = sc.next().charAt(0);
+		
 			sc.nextLine();
-			for (int i = 0; i < count; i++) {
-				System.out.println(i + 1 + "번 실패하였습니다. 5번 틀릴시 프로그램이 종료됩니다.");
-			}
+		
 			count++;
 			if(c == 'y' || c == 'Y') {
+					System.out.println(count + "번 실패하였습니다. 5번 틀릴시 프로그램이 종료됩니다.");
+				
 				break;
 			}else {
 				break label;
@@ -42,6 +44,9 @@ public class Login {
 			
 			
 	}
+		if(count == 5) {
+			System.out.println("5번 틀려서 접근 제한");
+		}
      
 	
 	
