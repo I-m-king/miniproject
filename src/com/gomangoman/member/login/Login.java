7561453d6d89a74455eac7ef6e68ebac75915543
GI_WON <rldnjsqhdl@naver.com>
2023-04-25 18:36:26 +0900

    기원 4

--- src/com/gomangoman/member/login/Login.java
+++ src/com/gomangoman/member/login/Login.java
@@ -12,8 +12,11 @@ public class Login {
 	
 	
 	public void login () {
-      label :
-	while(true) {
+      
+		int count = 0;
+		label :
+    
+		while(count>=5) {
 		System.out.print("아이디를 입력주세요 : ");
 		String id = sc.nextLine();
 		System.out.print("비밀번호를 입력해주세요 : ");
@@ -25,6 +28,7 @@ public class Login {
 			System.out.print("로그인에 실패했습니다 다시 입력하시겠습니까? Y/N");
 			char c = sc.next().charAt(0);
 			sc.nextLine();
+			count++;
 			if(c == 'y' || c == 'Y') {
 				break;
 			}else {
